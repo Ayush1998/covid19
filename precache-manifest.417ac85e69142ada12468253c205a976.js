43d5e4e67d36f36cbdf0c4b74eb8f7a1f05bc69f
Ayush Mudgal <ayushmudgal11@gmail.com>
2020-04-24 05:48:54 +0530

    Updates

---
+++ precache-manifest.417ac85e69142ada12468253c205a976.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4712af2f171c5124702b19c62f50c724",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "db54eaeb6608c8b46060",
+    "url": "/covid19/static/css/main.1c391a85.chunk.css"
+  },
+  {
+    "revision": "befe2201f4aa7baa53e5",
+    "url": "/covid19/static/js/2.db550a50.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19/static/js/2.db550a50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db54eaeb6608c8b46060",
+    "url": "/covid19/static/js/main.35df6ff0.chunk.js"
+  },
+  {
+    "revision": "cdfcea41dfab553d9d0f",
+    "url": "/covid19/static/js/runtime-main.9f6017dd.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19/static/media/covid19.d7265326.png"
+  }
+]);
